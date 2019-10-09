1146211a458d66ebe3bcfc2d618631672d329a6c
GeorgoSK <juraj.kovac@somemove.cz>
2019-10-09 16:33:25 +0200

    Refinements in content

--- eshop.js
+++ eshop.js
@@ -7,9 +7,24 @@ function updateFares(pax, currency = "Kč") {
 	});
 }
 
-function calculatePrice(dist, cls = 2) {
-	var fare = Math.round(1.3354 * dist + 11.498);
-	return (cls == 1) ? fare * 1.3 : fare;
+function calculatePrice(dist, cls = 2, val = 'single', ret = false) {
+	const base = 1.3354;
+	const fee = ret ? 11.935 : 11.498;
+	var fare = ret ? Math.round(2 * (base * dist + fee) * 0.95) : Math.round(base * dist + fee);
+
+	switch (val) {
+		case 'single':
+			return (cls == 1) ? Math.round(fare * 1.3) : fare;
+		case 'weekly':
+			fare = 8 * fare;
+			return (cls == 1) ? Math.round(fare * 1.2) : fare;
+		case 'monthly':
+			fare = 28 * fare;
+			return (cls == 1) ? Math.round(fare * 1.2) : fare;
+		case 'quarterly':
+			fare = 74 * fare;
+			return (cls == 1) ? Math.round(fare * 1.2) : fare;
+	}
 }
 
 function typeIn(text, field) {
@@ -21,7 +36,7 @@ function typeIn(text, field) {
 	}, 80);
 }
 
-$(document).ready(() => {
+$(() => {
 	
 	var currentDate = new Date();
 	var paxNumber = 1;
@@ -115,9 +130,24 @@ $(document).ready(() => {
 	setTimeout(() => {
 		typeIn(destination, $('#search-destination'));
 		$('.search-submit').click();
-		$('#search-results article')[1].click();
+		$('#search-results article').first().click();
 	}, 1000);
 	/*
 	$('#result-continue').click();
 	*/
+
+	if ($('body').hasClass('calculator')) {
+		$('#sjt-km').val(Math.round(Math.random()*200));
+		$('#results').hide();
+		$('#calculate').on('click', function() {
+			var cls = $('input[name=class]:checked').val();
+			var dist = parseInt($('#sjt-km').val());
+			$('#results').slideDown();
+			$('#single').html(calculatePrice(dist, cls) + " Kč");
+			$('#return').html(calculatePrice(dist, cls, 'single', true) + " Kč");
+			$('#weekly').html(calculatePrice(dist, cls, 'weekly') + " Kč");
+			$('#monthly').html(calculatePrice(dist, cls, 'monthly') + " Kč");
+			$('#quarterly').html(calculatePrice(dist, cls, 'quarterly') + " Kč");
+		});
+	}
 });
